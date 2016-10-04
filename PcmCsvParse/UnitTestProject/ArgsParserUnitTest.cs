6f445dd1074440c0dc8135a1700bb7b78f587456
Alexander Bodalevsky <abodalevsky@lohika.com>
2016-10-04 12:41:28 +0300

    Added parser of arguments of command line

---
+++ PcmCsvParse/UnitTestProject/ArgsParserUnitTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using pcmcsvparse;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class ArgsParserUnitTest
+    {
+        [TestMethod]
+        public void EmptyArgsNegative()
+        {
+            string[] args1 = { };
+            var pars1 = new ArgsParser(args1);
+            Assert.IsTrue(String.IsNullOrEmpty(pars1.GetFileName()));
+
+            string[] args2 = {"-K", "DD" };
+            var pars2 = new ArgsParser(args2);
+            Assert.IsTrue(String.IsNullOrEmpty(pars2.GetFileName()));
+
+            string[] args3 = { "-f"};
+            var pars3 = new ArgsParser(args3);
+            Assert.IsTrue(String.IsNullOrEmpty(pars3.GetFileName()));
+        }
+
+        [TestMethod]
+        public void ParseFileName()
+        {
+            string[] args = { "-f", "file.name" };
+            var pars = new ArgsParser(args);
+            Assert.AreEqual("file.name", pars.GetFileName());
+
+            string[] args1 = { "-F", "file.name" };
+            var pars1 = new ArgsParser(args1);
+            Assert.AreEqual("file.name", pars1.GetFileName()); // case insensitive
+        }
+
+        [TestMethod]
+        public void ParseParameters()
+        {
+            string[] args = { "-p", "MISS2", "0.045", "-P", "MISS3", "100.04" };
+            var pars = new ArgsParser(args);
+            Assert.AreEqual(2, pars.Parameters.Count);
+            Assert.AreEqual("MISS3", pars.Parameters[1].Key);
+            Assert.AreEqual(0.045f, pars.Parameters[0].Value);
+        }
+
+        [TestMethod]
+        public void ParseFullSet()
+        {
+            string[] args = { "-f", "file.name", "-p", "MISS2", "0.045", "-P", "MISS3", "100.04" };
+            var pars = new ArgsParser(args);
+            Assert.AreEqual("file.name", pars.GetFileName()); // case insensitive
+            Assert.AreEqual(2, pars.Parameters.Count);
+            Assert.AreEqual("MISS2", pars.Parameters[0].Key);
+            Assert.AreEqual(100.04f, pars.Parameters[1].Value);
+
+        }
+
+
+    }
+}
---
+++ PcmCsvParse/pcmcsvparse/ArgsParser.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace pcmcsvparse
+{
+    public class ArgsParser
+    {
+        string _fileName;
+
+        public List<KeyValuePair<string,float>> Parameters
+        {
+            get;
+            private set;
+        }
+
+        public ArgsParser(string[] args)
+        {
+            Parameters = new List<KeyValuePair<string, float>>();
+            for (int i = 0; i < args.Length;)
+            {
+                string token = args[i];
+
+                if (!parseParam(token, args, ref i))
+                    break;
+            }
+        }
+
+        bool parseParam(string token, string[] args, ref int idx)
+        {
+            if (token.ToLower() == "-f")
+            {
+                if (idx + 1 < args.Length)
+                {
+                    _fileName = args[idx + 1];
+                    idx += 2;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else if (token.ToLower() == "-p")
+            {
+                if (idx + 2 < args.Length)
+                {
+                    string par = args[idx + 1];
+                    string val = args[idx + 2];
+                    idx += 3;
+
+                    float f = 0;
+                    Single.TryParse(val, out f);
+                    Parameters.Add(new KeyValuePair<string, float>(par, f));
+
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return false;
+        }
+
+        public string GetFileName()
+        {
+            return _fileName;
+        }
+    }
+}
