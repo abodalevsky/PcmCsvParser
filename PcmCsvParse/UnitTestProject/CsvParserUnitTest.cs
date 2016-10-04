e8a77167b34eb8d819b4b4428ff97e1d1aa5727a
Alexander Bodalevsky <abodalevsky@lohika.com>
2016-10-04 11:43:49 +0300

    Added CSV parser

---
+++ PcmCsvParse/UnitTestProject/CsvParserUnitTest.cs
@@ -0,0 +1,201 @@
+﻿using Ideafixxxer.CsvParser;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class CsvParserUnitTest
+    {
+        [TestMethod()]
+        public void EmptyTest()
+        {
+            string[][] actual = Parse("", true, 10);
+            Assert.AreEqual(0, actual.Length);
+        }
+
+        [TestMethod()]
+        public void EmptyLineTest()
+        {
+            string[][] actual = Parse("\r\n", false, 10);
+            Assert.AreEqual(1, actual.Length);
+            Assert.AreEqual(0, actual[0].Length);
+        }
+
+        [TestMethod()]
+        public void SingleCommaTest()
+        {
+            string[][] actual = Parse(",", false, 10);
+            Assert.AreEqual(1, actual.Length);
+            AreEqual(new[] { "", "" }, actual[0]);
+        }
+
+        [TestMethod()]
+        public void QuoteCharTest()
+        {
+            string[][] actual = Parse("a\"b", false, 10);
+            Assert.AreEqual(1, actual.Length);
+            AreEqual(new[] { "a\"b" }, actual[0]);
+        }
+
+        [TestMethod()]
+        public void QuoteCharTest2()
+        {
+            string[][] actual = Parse("a\"", false, 10);
+            Assert.AreEqual(1, actual.Length);
+            AreEqual(new[] { "a\"" }, actual[0]);
+        }
+
+        [TestMethod()]
+        public void QuoteCharTest3()
+        {
+            string[][] actual = Parse("a\",b", false, 10);
+            Assert.AreEqual(1, actual.Length);
+            AreEqual(new[] { "a\"", "b" }, actual[0]);
+        }
+
+        [TestMethod()]
+        public void EmptyLineTestWithTrimTrailingEmptyLines()
+        {
+            string[][] actual = Parse("\r\n", true, 10);
+            Assert.AreEqual(0, actual.Length);
+        }
+
+        [TestMethod()]
+        public void SingleLineTest()
+        {
+            string[][] actual = Parse("a,b,c", true, 10);
+            Assert.AreEqual(1, actual.Length);
+            AreEqual(new[] { "a", "b", "c" }, actual[0]);
+        }
+
+        [TestMethod()]
+        public void SingleLineTest2()
+        {
+            string[][] actual = Parse("a,b,c\r\n", true, 10);
+            Assert.AreEqual(1, actual.Length);
+            AreEqual(new[] { "a", "b", "c" }, actual[0]);
+        }
+
+        [TestMethod()]
+        public void TwoLinesTest()
+        {
+            string[][] actual = Parse("a,b\r\nc,d", true, 10);
+            Assert.AreEqual(2, actual.Length);
+            AreEqual(new[] { "a", "b" }, actual[0]);
+            AreEqual(new[] { "c", "d" }, actual[1]);
+        }
+
+        [TestMethod()]
+        public void QuotedValueTest()
+        {
+            string[][] actual = Parse("a,\"b\",c", true, 10);
+            Assert.AreEqual(1, actual.Length);
+            AreEqual(new[] { "a", "b", "c" }, actual[0]);
+        }
+
+        [TestMethod()]
+        public void SingleQuotedValueTest()
+        {
+            string[][] actual = Parse("\"b\"", true, 10);
+            Assert.AreEqual(1, actual.Length);
+            AreEqual(new[] { "b" }, actual[0]);
+        }
+
+        [TestMethod()]
+        public void EmptyQuotedValueTest()
+        {
+            string[][] actual = Parse("a,\"\",c", true, 10);
+            Assert.AreEqual(1, actual.Length);
+            AreEqual(new[] { "a", "", "c" }, actual[0]);
+        }
+
+        [TestMethod()]
+        public void EmptyValueTest()
+        {
+            string[][] actual = Parse("a,,c", true, 10);
+            Assert.AreEqual(1, actual.Length);
+            AreEqual(new[] { "a", "", "c" }, actual[0]);
+        }
+
+        [TestMethod()]
+        public void QuotedValueWithCommaTest()
+        {
+            string[][] actual = Parse("a,\"b,c\",d", true, 10);
+            Assert.AreEqual(1, actual.Length);
+            AreEqual(new[] { "a", "b,c", "d" }, actual[0]);
+        }
+
+        [TestMethod()]
+        public void QuotedValueWithQuoteTest()
+        {
+            string[][] actual = Parse("a,\"b\"\"c\",d", true, 10);
+            Assert.AreEqual(1, actual.Length);
+            AreEqual(new[] { "a", "b\"c", "d" }, actual[0]);
+        }
+
+        [TestMethod()]
+        public void QuotedValueWithEOLTest()
+        {
+            string[][] actual = Parse("a,\"b\r\nc\",d", true, 10);
+            Assert.AreEqual(1, actual.Length);
+            AreEqual(new[] { "a", "b\r\nc", "d" }, actual[0]);
+        }
+
+        [TestMethod()]
+        public void TrimTrailingEmptyLinesTest()
+        {
+            string[][] actual = Parse("a,b,c\r\n,,", true, 10);
+            Assert.AreEqual(1, actual.Length);
+            AreEqual(new[] { "a", "b", "c" }, actual[0]);
+        }
+
+        [TestMethod()]
+        public void DontTrimTrailingEmptyLinesTest()
+        {
+            string[][] actual = Parse("a,b,c\r\n,,", false, 10);
+            Assert.AreEqual(2, actual.Length);
+            AreEqual(new[] { "a", "b", "c" }, actual[0]);
+            AreEqual(new[] { "", "", "" }, actual[1]);
+        }
+
+        [TestMethod()]
+        public void TrimTrailingEmptyLinesWithMaxColumnsTest()
+        {
+            string[][] actual = Parse("a,b,c\r\n,,d", true, 2);
+            Assert.AreEqual(1, actual.Length);
+            AreEqual(new[] { "a", "b" }, actual[0]);
+        }
+
+        [TestMethod()]
+        public void MaxColumnsTest()
+        {
+            string[][] actual = Parse("a,b,c", true, 2);
+            Assert.AreEqual(1, actual.Length);
+            AreEqual(new[] { "a", "b" }, actual[0]);
+        }
+
+        private void AreEqual(string[] one, string[] two)
+        {
+            Assert.AreEqual(one.Length, two.Length);
+            for (int i = 0; i < one.Length; i++)
+            {
+                Assert.AreEqual(one[i], two[i]);
+            }
+        }
+
+        private string[][] Parse(string input, bool trimTrailingEmptyLines, int maxColumnsToRead)
+        {
+            CsvParser2 parser = new CsvParser2()
+            {
+                TrimTrailingEmptyLines = trimTrailingEmptyLines,
+                MaxColumnsToRead = maxColumnsToRead
+            };
+
+            using (StringReader reader = new StringReader(input))
+            {
+                return parser.Parse(reader);
+            }
+        }
+    }
+}
---
+++ PcmCsvParse/UnitTestProject/PcmCsvParserUnitTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using pcmcsvparse;
+using System.IO;
+using System.Collections.Generic;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class PcmCsvParserUnitTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void OpenFileNegative()
+        {
+            var parser = new PcmCsvParser("nofile");
+        }
+
+        [TestMethod]
+        public void OpenFileSuccess()
+        {
+            var parser = new PcmCsvParser("..//..//Fixtures//test.csv");
+        }
+
+        [TestMethod]
+        public void HasColumn()
+        {
+            var parser = new PcmCsvParser("..//..//Fixtures//test.csv");
+            Assert.IsTrue(parser.HasColumn("L2MISS"));
+            Assert.IsFalse(parser.HasColumn("L2KISS"));
+            Assert.IsTrue(parser.HasColumn("L2MiSs")); //case insensitive
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(KeyNotFoundException))]
+        public void GetMaxNegative()
+        {
+            var parser = new PcmCsvParser("..//..//Fixtures//test.csv");
+            parser.GetMax("WRONG_COLUMN_NAME");
+        }
+
+        [TestMethod]
+        public void GetMax()
+        {
+            var parser = new PcmCsvParser("..//..//Fixtures//test.csv");
+            Assert.AreEqual(parser.GetMax("L3MISS"), 1.22f);
+            Assert.AreEqual(parser.GetMax("FREQ"), 0.0541f);
+            Assert.AreEqual(parser.GetMax("DATE"), 0.0f); //Invalid format, should be ignored
+            Assert.AreEqual(parser.GetMax("INST"), 1080.0f);//Scientific form transformation (1.08E+03)
+        }
+
+
+    }
+}
