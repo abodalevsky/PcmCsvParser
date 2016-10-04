e0026e9e9a8a31cd2a8a3e199bf9043edb7adc3f
Alexander Bodalevsky <abodalevsky@lohika.com>
2016-10-04 13:12:54 +0300

    Added parsing to main flow

--- PcmCsvParse/pcmcsvparse/Program.cs
+++ PcmCsvParse/pcmcsvparse/Program.cs
@@ -8,8 +8,52 @@ namespace pcmcsvparse
 {
     class Program
     {
+        enum ExitCode : int
+        {
+            Success = 0,
+            WrongArguments = 1,
+            ExceededThreshold = 2
+        };
+
         static void Main(string[] args)
         {
+            if (args.Length < 5)
+            {
+                printBanner();
+                Environment.Exit((int)ExitCode.WrongArguments);
+            }
+
+            try
+            {
+                var argsParser = new ArgsParser(args);
+                var csvParser = new PcmCsvParser(argsParser.GetFileName());
+
+                var metrix = argsParser.Parameters;
+                foreach(var metric in metrix)
+                {
+                    var realValue = csvParser.GetMax(metric.Key);
+                    if (realValue > metric.Value)
+                    {
+                        Console.WriteLine($"METRIC: {metric.Key} exceeded threshhold {metric.Value} (real: {realValue})");
+                        Environment.Exit((int)ExitCode.ExceededThreshold);
+                    }
+                }
+
+                Console.WriteLine("Ok");
+                Environment.Exit((int)ExitCode.Success);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error during parsing {e.Message}");
+                Environment.Exit((int)ExitCode.ExceededThreshold);
+            }
+
+        }
+
+        static void printBanner()
+        {
+            Console.WriteLine("pcmcsvparse -f filename -p PAR_NAME PAR_VALUE [-p PAR_NAME PAR_VALUE]");
         }
     }
 }
